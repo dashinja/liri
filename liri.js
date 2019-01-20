dcced8fbc7c33829e244f414733784c7e808d985
DaShinja <byron@dashinja.com>
2019-01-19 22:43:11 -0500

    cleanup code

--- liri.js
+++ liri.js
@@ -55,7 +55,7 @@ function movie() {
       let actors = data.Actors;
       let plot = data.Plot;
 
-      let results = `${title} debuted primarily in ${language} on ${releaseYear} in ${country} with a ${ratingIMDB} rating from IMDB.\n The actors include ${actors}, and who ever else to boot.\nPlot: ${plot}`;
+      let results = `${title} debuted primarily in ${language} on ${releaseYear} in ${country} with a ${ratingIMDB} rating from IMDB.\n The actors include ${actors}, and who ever else to boot.\nPlot: ${plot}\n----------------------------------------------------------------------------\n\n`;
       console.log(results);
       writeToLog(results);
     })
@@ -129,31 +129,12 @@ function concert() {
 function doWhatItSays() {
   fs.readFile('./random.txt', 'utf8', (err, data) => {
     if (err) throw err;
-    console.log("I'm raw data read:\r", data);
     let dataSplit = data.split('\n');
     let dataSplit10 = data.split(', ');
-    console.log("I'm data split", dataSplit);
-    console.log("I'm data split TEN:", dataSplit10);
-
-    // let calmer = dataSplit10.filter((item) => item === "spotify"|| item === "concert"|| item === "movie")
-    // console.log("I'm calmer:", calmer)
-
-    operation = dataSplit10[0];
-    console.log("I'm changed operation:", operation);
-    queryCLI = dataSplit10[1];
-    console.log("I'm changed queryCLI:", queryCLI, '\n');
-    console.log("I'm length of datasplit10", dataSplit10.length);
-    console.log(
-      "I'm length of datasplit10 minus one",
-      dataSplit10.length - 1,
-      '\n'
-    );
 
     for (let i = 0; i < dataSplit10.length; i += 2) {
       operation = dataSplit10[i];
-      console.log("I'm operation now:", operation);
-      queryCLI = dataSplit10[i + 1];
-      console.log("I'm querycli now:", queryCLI);
+      process.argv[3] = dataSplit10[i + 1];
       OPERATOR[operation]();
     }
   });
